615d088d695027c00fe846f582d68db2fb40cdac
fellipebs <49615183+fellipebs@users.noreply.github.com>
2019-04-27 21:34:40 -0300

    Enviando inicial page

---
+++ inicialpage.php
@@ -0,0 +1,52 @@
+﻿<html>
+<head>
+ <!-- BOOTSTRAP 3 -->
+ <!-- <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script> -->
+    <!-- BOOTSTRAP 4 -->
+    <?php require_once 'conexao/conexao.php'; ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" type="text/css" src="css/styles.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Easy Learning (Trocar!)</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+ 
+  <ul class="nav navbar-nav navbar-right">
+      <li><span style='color: white;'>
+<?php
+session_start();
+echo $_SESSION['usuario']->usuario;
+$foto = $_SESSION['usuario']->foto;
+?>
+      </span></li>
+      <li>
+      <?php
+     echo" <img src='img/$foto' class='rounded-circle' width='50px' height='50px'>";
+      ?>
+      </li>
+     
+    </ul>
+</nav>
+<center>
+<h2 class='center'>Ok!</h2></center>
+<br>
+<!-- visualizacao -->
+
+
+
+</body>
+</html>
