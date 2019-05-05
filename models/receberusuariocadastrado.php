a77dc6645e027d4fa730877aa3e7b48e67b05486
fellipebs <49615183+fellipebs@users.noreply.github.com>
2019-05-05 12:46:50 -0300

    Atualizando Foto do cadastro

--- models/receberusuariocadastrado.php
+++ models/receberusuariocadastrado.php
@@ -6,26 +6,26 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 
 
-if(isset($_FILES['foto']))
-{
-   $ext = strtolower(substr($_FILES['foto']['name'],-4)); //Pegando extensão do arquivo
-   $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-   $dir = './img/'; //Diretório para uploads 
-   move_uploaded_file($_FILES['foto']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
-   echo("Imagem enviada com sucesso!");
-} 
+// if(isset($_FILES['foto']))
+// {
+//    $ext = strtolower(substr($_FILES['foto']['name'],-4)); //Pegando extensão do arquivo
+//    $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+//    $dir = './img/'; //Diretório para uploads 
+//    move_uploaded_file($_FILES['foto']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
+//    echo("Imagem enviada com sucesso!");
+// } 
 
 // Codigo para Resgatar a mensagem da página
 
 
-   
-$stmt = $pdo->prepare('INSERT INTO usuarios (Usuario, email, Login, Senha, foto) VALUES(:Usuario, :email, :Login, md5(:Senha), :foto )');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$stmt = $pdo->prepare('INSERT INTO usuarios (Usuario, email, Login, Senha) VALUES(:Usuario, :email, :Login, md5(:Senha))');
 $stmt->execute(array(
   ':Usuario' => $usuario,
   ':email' => $email,
   ':Login' => $login,
-  ':Senha' => $senha,
-  ':foto' => $new_name
+  ':Senha' => $senha
+
   
 ));
 
