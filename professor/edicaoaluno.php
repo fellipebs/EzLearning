51a656f3eba66d8dbd081b6f23a70e7f2a826cf9
fellipebs <49615183+fellipebs@users.noreply.github.com>
2019-05-08 22:40:27 -0300

    Atualizações na visualização do professor

---
+++ professor/edicaoaluno.php
@@ -0,0 +1,125 @@
+<?php
+
+$teste = $_POST['id'];
+session_start();
+
+require_once ('../models/conexao/conexao.php');  
+$sql = $con->prepare("SELECT * FROM usuarios where id = '$teste'");
+$sql->execute();
+$rows = $sql->fetchAll(PDO::FETCH_CLASS);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8">
+
+  <title>Easy Learning</title> 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+
+  <link rel="stylesheet" type="text/css" href="css/csshome.css"> 
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  <link rel="shortcut icon" href="Imagens/iconesite" />
+  
+
+    <style>
+
+    html, body {
+      background-image: url("imagens/backgroundloginblur.png");
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      background-size: cover;  
+      }
+
+    
+    .team{
+      background-color: #2f2f2f;
+      color: #ffffff;
+    }
+
+    .imgperfil{
+        width: 200px;
+        height: 200px;
+        background: transparent;
+        border: 2px solid #2f2f2f;
+        }
+      
+        .borda1{
+      border-top-width: thin;
+      border-top-style: solid;
+      border-top-color: #ffffff;
+      }
+
+        .borda2{
+      border-bottom-width: thin ;
+      border-bottom-style: solid;
+      border-bottom-color: #ffffff;
+      }
+      
+        
+
+    </style>
+</head>
+
+<body>
+<div class="container">
+    <h1>Editar Perfil do aluno</h1>
+    <?php foreach ($rows as $row): ?>
+    <div class="row">
+      <!-- left column -->
+      <div class="col-md-3">
+      <form method="post" enctype="multipart/form-data" action="receberalunoeditado.php">
+        <div class="text-center">
+          <img src="../assets/images/user/<?php echo $row->foto; ?>" class="avatar img-circle" alt="avatar" style="width: 100px; height: 100px;">
+         
+          
+         </div>
+    </div>
+      <!-- edit form column -->
+      <div class="col-md-9 personal-info">
+        <h3>Alterar Perfil</h3>
+          <div class="form-group">
+          
+          </div>
+         
+          <div class="form-group">
+            <label class="col-md-3 control-label">Usuário do aluno</label>
+            <div class="col-md-8">
+              <input class="form-control" type="text" value="<?php echo $row->usuario; ?>" name="usuario">
+            </div>
+          </div>
+          <input type='hidden' name='id' id='id' value='<?php echo $row->id; ?>'>
+          <div class="form-group">
+            <label class="col-md-3 control-label">Senha:</label>
+            <div class="col-md-8">
+              <input class="form-control" type="password" value="<?php echo $row->senha; ?>" name="senha">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="col-md-3 control-label"></label>
+            <div class="col-md-8">
+              <input type="submit" class="btn btn-primary" value="Salvar Alterações">
+              <span></span>
+              <input type="reset" class="btn btn-default" value="Cancelar">
+            </div>
+          </div>
+        </form>
+        <?php endforeach; ?>
+      </div>
+  </div>
+</div>
+
+
+            </div>
+        </div>
+    </body>
+</html>
--- professor/index.php
+++ professor/index.php
@@ -1,1 +1,97 @@
-<?php echo "Professor"; ?>
+<?php
+    session_start();
+    require_once ('../models/conexao/conexao.php'); 
+    $sql= $con->prepare("SELECT id,nome,sobrenome,DATE_FORMAT(dt_nascimento,'%d/%m/%Y')as dt_nascimento,responsavel, usuario_id_aluno FROM aluno; ");
+    $sql->execute();
+
+    $rows = $sql->fetchAll(PDO::FETCH_CLASS);
+    require("../models/restrito.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Easy Learning - Alunos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/normalize.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/grid.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,400i" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/main.css">
+</head>
+
+<body>
+    
+    <div class="row">
+        <h3>alunos</h3>
+    </div>
+    <form action="">
+        <div class="row">
+            <div class="col span-1-of-5">
+                <input type="text" name="nome" placeholder="Nome">
+            </div>
+            <div class="col span-1-of-5">
+                <input type="text" name="sobrenome" placeholder="Sobrenome">
+            </div>
+            <div class="col span-1-of-5">
+                <input type="text" name="nascimento" placeholder="Data de Nascimento">
+            </div>
+            <div class="col span-1-of-5">
+                <input type="text" name="responsavel" placeholder="Nome do Responsavel">
+            </div>
+            <div class="col span-1-of-5">
+                <input type="submit" value="cadastrar" class="btn btn-full">
+            </div>
+        </div>
+    </form>
+    
+    <div class="row">
+        <table>
+            <thead>
+                <tr>
+                <th scope="col"></th>
+                <th scope="col">Numero</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Sobrenome</th>
+                <th scope="col">Data de Nascimento</th>
+                <th scope="col">Responsável</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($rows as $row): ?>
+                <form action="edicaoaluno.php" method="post">
+               
+                <tr>
+                <th scope="row">
+                    <input type='submit' value='Editar!'></input>
+                </th>
+                <th>
+                    <?php echo $row->id; ?>
+                </th>
+                <td>
+                    <?php echo $row->nome; ?>
+                </td>
+                <td>
+                    <?php echo $row->sobrenome; ?>
+                </td>
+                <td>
+                    <?php echo $row->dt_nascimento; ?>
+                </td>
+                <td>
+                    <?php echo $row->responsavel; ?>
+                  <input type='hidden' name='id' id='id' value='<?php echo $row->usuario_id_aluno; ?>'>
+                </td>
+                </tr>
+               
+                </form>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+</body>
+
+</html>
