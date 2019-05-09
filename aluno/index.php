45abe09fae0bf1378a389265f15b978222c77b1b
rafaeller <rafaelecmarques@gmail.com>
2019-05-08 22:24:21 -0300

    aluno index.php estilizado

--- aluno/index.php
+++ aluno/index.php
@@ -1,56 +1,68 @@
 <?php 
-echo "Aluno"; 
 header("Content-type:text/html; charset=utf8");
+require_once ('../models/conexao/conexao.php');
 session_start();
+$sql= $con->prepare("SELECT id,nota,codigo FROM atividades; ");
+$sql->execute();
+$rows = $sql->fetchAll(PDO::FETCH_CLASS);
+
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 
-<meta charset="utf-8">
-
-  <title>Easy Learning</title>
-
-  <meta name="description" content="Aprenda tudo sobre a história da música e seus estilos"/>
-	<meta name="keywords" content="Música, Eletrônica, Lo-Fi, Blog, Sertanejo, Historia, Rock, Estilos Musicas, Informação"/>
-  <meta name="author" content="Davi Cecílio"> <!-- FRONT-END -->
+  <title>Easy Learning - Alunos</title>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="css/csshome.css"> 
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-  <link rel="shortcut icon" href="Imagens/iconesite" />
+  <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/normalize.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/grid.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,400i" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" media="screen" href="../assets/css/main.css">
   
 </head>
 
 <body>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-    <div class="container">
-    <ul class="nav navbar-nav" >
-        <li><img  class="rounded-circle" height="50px" src="../assets/images/user/<?php  echo $_SESSION['foto'];?>" alt=""></li>
-        <li style="margin-left: 30px; margin-top: 5px;" class="active" style="margin-left: 30px;"><a href="#" class="navbar-brand"><?php  echo " ".$_SESSION['usuario']->usuario;?></a></li>
-      </ul>
-      <a class="navbar-brand" href="bloghome.php">HOME</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
-        aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-          <a class="nav-link" href="alterar-perfil.php">
-              Alterar Perfil
-          </a>
-          </li>
-        </ul>
-
-      </div>
-    </div>
+   <nav class="padding-nav">
+   <div class="logo-black">Easy Learning</div>
+    <ul class="main-nav nav-black">
+      <li>
+        <img  class="rounded-circle" height="50px" src="../assets/images/user/<?php  echo $_SESSION['foto'];?>" alt="">
+        <a href="#" class="navbar-brand"><?php echo $_SESSION['usuario']->usuario;?></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="alterar-perfil.php">Alterar Perfil</a>
+      </li>
+    </ul>
   </nav>
+  <div class="clearfix"></div>
+  <div class="row fluid-row">
+    <?php foreach ($rows as $row): ?>
+        <a href="">
+            <div class="card">
+                <img src="../assets/images/algorithm.png" alt="Avatar">
+                <div class="card-content">
+                    <h4>Atividade</h4> 
+                    <p>Nome da Atividade</p> 
+                </div>
+            </div>
+         </a>
+        <a href="">
+            <div class="card">
+                <img src="../assets/images/algorithm.png" alt="Avatar">
+                <div class="card-content">
+                    <h4>Atividade</h4> 
+                    <p>Nome da Atividade</p> 
+                </div>
+            </div>
+         </a>
+        
+    <?php endforeach; ?>
+    
+</div>
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
 </body>
     
 
