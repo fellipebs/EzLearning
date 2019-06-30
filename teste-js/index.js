618b0acd3646254903e91bd2e25f87cf4efdf7e3
fellipebs <49615183+fellipebs@users.noreply.github.com>
2019-06-30 11:19:57 -0300

    Att atividade platinha

--- teste-js/index.js
+++ teste-js/index.js
@@ -3,14 +3,27 @@ function teste(){
     if(contador == '0'){
         document.getElementById('planta').src='2.jpg';
         document.getElementById('contador').value='1';
+        alert('Parabéns, sua planta está crescendo corretamente!');
     }else if(contador == '1'){
        document.getElementById('planta').src='3.jpg';
        document.getElementById('contador').value='2';
-    }else{
+       alert('Continue regando para ver mais progresso!');
+    }else if(contador == '2'){
        document.getElementById('planta').src='4.jpg';
+       document.getElementById('contador').value='3';
+       document.getElementById('continuar').style='display: block;'
+       alert('Eba! Sua planta chegou ao estágio final!');
+    }else if(contador == '3'){
+        alert('Ai você matou a planta afogada...');
+        document.getElementById('continuar').style='display: none;'
+        document.getElementById('refresh').style='display: block;'
+        document.getElementById('contador').value='4';
+        document.getElementById('planta').src='5.jpg';
+    }else{
+        alert('Reinicie o jogo!');
     }
+}
 
-
-    contador++;
-
+function refresh(){
+    window.location.reload();
 }
