942c95d76c7c491907dea14fc681e577a9539bb4
Fellipe Barcelos <fellipebarcelossaraiva@gmail.com>
2019-07-02 19:58:31 -0300

    Iniciando a Página de Cadastro de Notificações

---
+++ professor/notificacao.php
@@ -0,0 +1,125 @@
+
+<?php
+    session_start();
+    require_once ('../models/conexao/conexao.php'); 
+    require("../models/restrito.php");
+    require_once("../componets/head.php");
+?>
+<?php
+if(isset($_POST['msg'])){
+	$msg = $_POST['msg'];
+	$aluno = $_POST['aluno'];
+	
+	
+	/*$pdo = new PDO("mysql:host=localhost; dbname=ezlearning;", "root", "");
+
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+$stmt = $pdo->prepare('INSERT INTO notificacao (descricao, aluno_id) VALUES(:descricao, :aluno_id)');
+$stmt->execute(array(
+  ':descricao' => $msg,
+  ':aluno_id' => $aluno
+));
+	*/
+
+}
+
+if(isset($_POST['check'])){
+	$teste = $_POST['check'];
+	echo $teste;
+}
+?>
+
+
+<body>
+    <?php require_once("../componets/menus.php");?>
+            <div class="breadcome-area">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="breadcome-list single-page-breadcome">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <div class="breadcome-heading">
+                                            <form role="search" class="sr-input-func">
+                                                <input type="text" placeholder="Pesquisar..." class="search-int form-control">
+                                                <a href="../#"><i class="fa fa-search"></i></a>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                        <ul class="breadcome-menu">
+                                            <li><a href="../#">Home</a> <span class="bread-slash">/</span>
+                                            </li>
+                                            <li><span class="bread-blod">Enviar notificações</span>
+                                            
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="product-status mg-b-15">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="product-status-wrap">
+                          
+                            <div class="asset-inner">
+                                <table>
+                                   Utilize esta página para enviar notificações aos alunos!
+                                   <br><br>
+                                   <input type="checkbox" name="check" value="" class=''>Selecione este check box para enviar a mensagem para todos da sala.<br>
+                                   
+                                   <br> Ou selecione o nome do aluno que você deseja notificar:
+                                   <select name='aluno' class='form-control'>
+                                    <option value='0'></option>
+                                    <?php
+// $con = new PDO("mysql:host=localhost; dbname=ezlearning;", "root","");
+// $sql = $con->prepare("SELECT * from aluno");
+// $sql->execute();
+
+// $rows = $sql->fetchAll(PDO::FETCH_CLASS);
+// //fetchAll - recupera todos os registros da tabela
+// //PDO::FETCH_CLASS - resultado � armazenado em um objeto
+
+// foreach ($rows as $row){
+// 	echo "<option value=".$row->id.">".$row->nome."</option>";
+                                // }
+                                    ?>
+  
+  
+  
+                                </select>
+                                    Escreva aqui a mensagem que estara na notificação:
+                                        <br>
+                                  <textarea name="msg" class='form-control'> </textarea>
+                                  <br>
+                                   <button>Cadastrar!</button>
+     
+                                 </form>
+                                </table>
+                            </div>
+                            <div class="custom-pagination">
+								<ul class="pagination">
+									<!-- <li class="page-item"><a class="page-link" href="../#">Previous</a></li>
+									<li class="page-item"><a class="page-link" href="../#">1</a></li>
+									<li class="page-item"><a class="page-link" href="../#">2</a></li>
+									<li class="page-item"><a class="page-link" href="../#">3</a></li>
+									<li class="page-item"><a class="page-link" href="../#">Next</a></li> -->
+								</ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php require_once("../componets/footer.php");?>
+        <?php require_once("../componets/js.php");?>
+</body>
+
+</html>
