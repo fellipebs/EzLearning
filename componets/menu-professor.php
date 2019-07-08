802fdf751dc655def3993d69514a1eb9645e8f84
Fellipe Barcelos <fellipebarcelossaraiva@gmail.com>
2019-07-08 18:45:50 -0300

    Atts

--- componets/menu-professor.php
+++ componets/menu-professor.php
@@ -66,18 +66,9 @@ require_once ('../models/conexao/conexao.php'); ?>
                         <li>
                             <a title="Landing Page" href="../professor/notificacao.php" aria-expanded="false"><span class="educate-icon educate-interface icon-wrap"></span> <span class="mini-click-non">Notificações</span></a>
                         </li>
-                        <!-- <li>
-                            <a class="has-arrow" href="../mailbox.html" aria-expanded="false"><span class="educate-icon educate-charts icon-wrap"></span> <span class="mini-click-non">Charts</span></a>
-                            <ul class="submenu-angle chart-mini-nb-dp" aria-expanded="false">
-                                <li><a title="Bar Charts" href="../bar-charts.html"><span class="mini-sub-pro">Bar Charts</span></a></li>
-                                <li><a title="Line Charts" href="../line-charts.html"><span class="mini-sub-pro">Line Charts</span></a></li>
-                                <li><a title="Area Charts" href="../area-charts.html"><span class="mini-sub-pro">Area Charts</span></a></li>
-                                <li><a title="Rounded Charts" href="../rounded-chart.html"><span class="mini-sub-pro">Rounded Charts</span></a></li>
-                                <li><a title="C3 Charts" href="../c3.html"><span class="mini-sub-pro">C3 Charts</span></a></li>
-                                <li><a title="Sparkline Charts" href="../sparkline.html"><span class="mini-sub-pro">Sparkline Charts</span></a></li>
-                                <li><a title="Peity Charts" href="../peity.html"><span class="mini-sub-pro">Peity Charts</span></a></li>
-                            </ul>
-                        </li> -->
+                        <li>
+                        <a title="Landing Page" href="../professor/listagemNotas.php" aria-expanded="false"><span class="educate-icon educate-charts icon-wrap"></span> <span class="mini-click-non">Notas</span></a>
+                        </li> 
                         <!-- <li>
                             <a class="has-arrow" href="../mailbox.html" aria-expanded="false"><span class="educate-icon educate-data-table icon-wrap"></span> <span class="mini-click-non">Data Tables</span></a>
                             <ul class="submenu-angle" aria-expanded="false">
