e09a63836720b068ce46012946c68bc485a4912d
Fellipe Barcelos <fellipebarcelossaraiva@gmail.com>
2019-08-02 19:50:58 -0300

    Lalalalala

--- componets/menu-professor.php
+++ componets/menu-professor.php
@@ -10,7 +10,7 @@ require_once ('../models/conexao/conexao.php'); ?>
                 <nav class="sidebar-nav left-sidebar-menu-pro">
                     <ul class="metismenu" id="menu1">
                         <li>
-                                <a title="Landing Page" href="home.php"" aria-expanded="false"><span class="educate-icon educate-home icon-wrap"></span> <span class="mini-click-non">Home</span></a>
+                                <a title="Landing Page" href="../index.html" aria-expanded="false"><span class="educate-icon educate-home icon-wrap"></span> <span class="mini-click-non">Home</span></a>
                         </li>
                         <!-- <li>
                             <a title="Landing Page" href="../events.html" aria-expanded="false"><span class="educate-icon educate-event icon-wrap sub-icon-mg" aria-hidden="true"></span> <span class="mini-click-non">Event</span></a>
@@ -235,7 +235,7 @@ require_once ('../models/conexao/conexao.php'); ?>
                                                     <?php
                                         
                                                     $sql = $con->prepare("SELECT a.id, a.descricao, a.aluno_id, DATE_FORMAT(a.data,'%d/%m') as data, u.usuario from notificacao a 
-                                                    inner join usuario u on a.aluno_id = u.id
+                                                    inner join usuarios u on a.aluno_id = u.id
                                                     where aluno_id = ".$_SESSION['usuario']->id."");
                                                      $sql->execute();
                                                     $linhas = $sql->fetchAll(PDO::FETCH_CLASS);
