f05b78a5daab07753755f8331435fb43ba122e92
Juan Colchete <juanudkp@gmail.com>
2019-09-16 21:56:28 -0300

    Atividade Contar

--- atividades/lacos/contar/index.php
+++ atividades/lacos/contar/index.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <?php require_once('../../components/head.php') ?>
-    <title>Atividade soma</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 </head>
 
 <body>
@@ -11,14 +12,14 @@
         <div class="row">
             <div class="col-md-3"></div>
             <div class="col-md-6">
-                <h1>Atividade soma</h1>
-                <p>Esta atividade tem como objetivo te ensinar a somar usando blocos, o objetivo desta atividade é somar dois números e imprimir o resultado na tela</p>
+                <h1>Atividade de Laços de Repetição uso do Contar</h1>
+                <p>Esta atividade tem como objetivo te ensinar a contar de 1 até 5.</p>
                 <br>
                 <h4>Exemplo</h4>
                 <div id="blocosDiv" class="area-demo"></div>
                 <br>
-                <p>O bloco definir (soma) cria a variável para guardar o valor da soma realizada pelo proximo bloco, o bloco imprime mostra na tela o valor da variável soma.</p>
-                <a href="soma.php" class="btn btn-success">Iniciar a atividade</a>
+                <p>O bloco contar a partir da variável i de 1 até 5 conta de 1 em 1, o proximo bloco imprime o valor da variável i na tela 5 vezes</p>
+                <a href="contar.php" class="btn btn-success">Iniciar a atividade</a>
             </div>
             <div class="col-md-3"></div>
         </div>
@@ -34,7 +35,7 @@
         trashcan: false
     });
 
-    readTextFile("../../demo/demo_soma.txt");
+    readTextFile("../../demo/demo_contar.txt");
     var xml = Blockly.Xml.textToDom(allText);
     Blockly.Xml.domToWorkspace(xml, workspace);
 </script>
