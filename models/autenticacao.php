b1c1f996ea98575dfd6010a449f71938eaa9ed1a
Juan Colchete <juanudkp@gmail.com>
2019-09-25 21:35:44 -0300

    Autenticar por email e login

--- models/autenticacao.php
+++ models/autenticacao.php
@@ -2,18 +2,18 @@
 <?php
 header('Content-type: text/html; charset=UTF-8');
 session_start();
-require_once ('conexao/conexao.php');
+require_once('conexao/conexao.php');
 
-$sql = $con->prepare("SELECT * FROM usuarios WHERE Login=? AND Senha=?");
-$sql->execute( array($_POST['login'], md5($_POST['senha']) ) );
+$sql = $con->prepare("SELECT * FROM usuarios WHERE Login=? AND Senha=? or Email=? AND Senha=?");
+$sql->execute(array($_POST['login'], md5($_POST['senha']), $_POST['login'], md5($_POST['senha'])));
 
 $row = $sql->fetchObject();  // devolve um �nico registro
 
 // Se o usu�rio foi localizado
-if ($row){
+if ($row) {
     $_SESSION['usuario'] = $row;
     $_SESSION['foto'] = $row->foto;
-    switch($_SESSION['usuario']->tipo){
+    switch ($_SESSION['usuario']->tipo) {
         case 0:
             header("Location: ../aluno");
             break;
@@ -24,7 +24,7 @@ if ($row){
             header("Location: ../educacional");
             break;
     }
-}else{
+} else {
     header("Location: ../login");
     $_SESSION['Alerta'] = '<div class="alert alert-danger alert-dismissible" role="alert">
     <strong>Falha no login</strong> Senha ou Usuário inválidos!
